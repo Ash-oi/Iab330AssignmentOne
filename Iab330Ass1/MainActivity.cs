926d30db254a3a2f76e2fe7c42ce40466d394d4c
Ashley <ash22334@hotmail.com>
2016-09-04 22:39:57 +1000

    initial commit

---
+++ Iab330Ass1/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Iab330Ass1
+{
+    [Activity(Label = "Latest Notices", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            
+        }
+    }
+}
+
